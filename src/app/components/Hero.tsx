0212a8e064536ea6da1cd1e67e63fc5200b3c147
Paweł Karniej <karniej.p@gmail.com>
2024-08-25 05:51:43 +0200

    updated version

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -23,6 +23,7 @@ import {
   useSpring,
   AnimatePresence,
 } from "framer-motion";
+import { FaApple } from "react-icons/fa";
 
 const MotionBox = motion(Box);
 const MotionHeading = motion(Heading);
@@ -220,6 +221,7 @@ const Hero: React.FC = () => {
                 width="100%"
                 maxWidth={{ base: "100%", sm: "400px" }}
               >
+                {/* 
                 <MotionInput
                   type="text"
                   placeholder="Enter your name"
@@ -294,6 +296,27 @@ const Hero: React.FC = () => {
                   }}
                 >
                   Join Waitlist
+                </Button> */}
+
+                <Button
+                  as="a"
+                  href="https://apps.apple.com/us/app/newsletterytics/id6505141597"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  leftIcon={<FaApple />}
+                  variant="solid"
+                  width="100%"
+                  colorScheme="brand"
+                  fontWeight="bold"
+                  my={2}
+                  bg={useColorModeValue("black", "white")}
+                  color={useColorModeValue("white", "black")}
+                  _hover={{
+                    bg: useColorModeValue("gray.800", "gray.200"),
+                  }}
+                  size="lg"
+                >
+                  Download on the App Store
                 </Button>
               </MotionBox>
               <a
@@ -398,17 +421,5 @@ const Hero: React.FC = () => {
     </Box>
   );
 };
-
-const AppleIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 384 512"
-    width="16"
-    height="16"
-    fill="currentColor"
-  >
-    <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
-  </svg>
-);
 
 export default Hero;
